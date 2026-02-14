c515c117b8cbd8a3ed0b3ed9ff4db3e063fa179e
Developer <dev@localhost>
2026-02-13 23:11:09 -0500

    feat(ios): add mpv native player views with PiP and background audio support
    Add iOS-native mpv player infrastructure to the PearTube app target:
    - MpvPlayerCore: mpv C API wrapper with async destroy (fixes freeze)
    - MpvPlayerView: UIView host with AVSampleBufferDisplayLayer
    - MpvPipController: AVPictureInPictureController integration
    - MpvHttpStreamBridge: local HTTP proxy for Hyperdrive streams
    - MpvPlayerManager/ViewManager: React Native bridge (ObjC)
    - Update bridging header, Xcode project, Podfile for mpv deps
    - Remove VLC references from Xcode project and Podfile

--- packages/app/ios/PearTube/PearTube-Bridging-Header.h
+++ packages/app/ios/PearTube/PearTube-Bridging-Header.h
@@ -1,3 +1,17 @@
 //
 // Use this file to import your target's public headers that you would like to expose to Swift.
 //
+
+#if __has_include(<React/RCTBridgeModule.h>)
+#import <React/RCTBridgeModule.h>
+#import <React/RCTEventEmitter.h>
+#import <React/RCTUIManager.h>
+#import <React/RCTViewManager.h>
+#endif
+
+#if __has_include(<Libmpv/client.h>)
+#import <Libmpv/client.h>
+#import <Libmpv/render.h>
+#import <Libmpv/render_gl.h>
+#import <Libmpv/stream_cb.h>
+#endif

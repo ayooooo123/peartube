a30be05334fb2960583731422473af6202978dd6
Developer <dev@localhost>
2026-02-20 14:32:28 -0500

    feat(cli): add CLI entry points with paparam argument parsing

---
+++ packages/cli/bin.js
@@ -0,0 +1,27 @@
+#!/usr/bin/env node
+
+import { command, flag, arg } from 'paparam'
+import goodbye from 'graceful-goodbye'
+
+const cmd = command('peartube-peer',
+  flag('--storage|-s', 'Storage path', { default: './peartube-peer' }),
+  flag('--max-storage|-m', 'Max storage in MB', { default: 100000 }),
+  flag('--channel|-c', 'Pin a channel key (can be repeated)', { multiple: true }),
+  flag('--debug|-d', 'Enable debug logging'),
+  async function ({ flags }) {
+    const { startPeer } = await import('./src/index.js')
+
+    const storagePath = flags.storage
+    const maxStorageMB = Number(flags['max-storage'])
+    const pinnedChannels = flags.channel || []
+    const debug = !!flags.debug
+
+    const peer = await startPeer({ storagePath, maxStorageMB, pinnedChannels, debug })
+
+    goodbye(async () => {
+      await peer.close()
+    })
+  }
+)
+
+cmd.parse()

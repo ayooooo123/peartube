3bdb1f66efd79491a79146ec00ff4b4d11a8a138
Developer <dev@localhost>
2026-03-20 13:14:08 -0400

    refactor: route mobile rpc through shared bridge

--- packages/protocol/src/event-map.js
+++ packages/protocol/src/event-map.js
@@ -1,6 +1,7 @@
 export const PROTOCOL_EVENTS = Object.freeze({
   HOST_READY: 'host.ready',
   HOST_ERROR: 'host.error',
+  LOG: 'log',
   UPLOAD_PROGRESS: 'upload.progress',
   DOWNLOAD_PROGRESS: 'download.progress',
   FEED_UPDATED: 'feed.updated',
@@ -15,6 +16,7 @@ export const PROTOCOL_EVENTS = Object.freeze({
 export const PROTOCOL_EVENT_BINDINGS = Object.freeze([
   ['onEventReady', PROTOCOL_EVENTS.HOST_READY],
   ['onEventError', PROTOCOL_EVENTS.HOST_ERROR],
+  ['onEventLog', PROTOCOL_EVENTS.LOG],
   ['onEventUploadProgress', PROTOCOL_EVENTS.UPLOAD_PROGRESS],
   ['onEventDownloadProgress', PROTOCOL_EVENTS.DOWNLOAD_PROGRESS],
   ['onEventFeedUpdate', PROTOCOL_EVENTS.FEED_UPDATED],
--- packages/protocol/src/index.js
+++ packages/protocol/src/index.js
@@ -1,2 +1,3 @@
-// Shared protocol exports are added in later tasks.
-export {}
+export * from './event-map.js'
+export * from './create-client.js'
+export { HOST_ERROR_CODES, PROTOCOL_VERSION } from '@peartube/host'

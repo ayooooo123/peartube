df69f46aaa50c98dd9dfac0943f8d0ff4f553fcc
Developer <dev@localhost>
2026-03-20 13:24:14 -0400

    refactor: share protocol bridge with pear desktop

--- packages/app/pear-src/worker-client.js
+++ packages/app/pear-src/worker-client.js
@@ -3,17 +3,28 @@
  *
  * Uses ES modules - load with <script type="module" src="./worker-client.js">
  */
-import HRPC from '@peartube/spec'
 import pipe from 'pear-pipe'
+import { createProtocolClient, PROTOCOL_EVENTS } from '@peartube/protocol'
+
+const DOM_EVENT_NAMES = Object.freeze({
+  [PROTOCOL_EVENTS.UPLOAD_PROGRESS]: 'pearUploadProgress',
+  [PROTOCOL_EVENTS.FEED_UPDATED]: 'pearFeedUpdate',
+  [PROTOCOL_EVENTS.VIDEO_STATS]: 'pearVideoStats',
+  [PROTOCOL_EVENTS.CAST_DEVICE_FOUND]: 'pearCastDeviceFound',
+  [PROTOCOL_EVENTS.CAST_DEVICE_LOST]: 'pearCastDeviceLost',
+  [PROTOCOL_EVENTS.CAST_PLAYBACK_STATE]: 'pearCastPlaybackState',
+  [PROTOCOL_EVENTS.CAST_TIME_UPDATE]: 'pearCastTimeUpdate'
+})
 
 class WorkerClient {
   constructor() {
     this.pipe = null
+    this.client = null
     this.rpc = null
     this.isConnected = false
     this._initPromise = null
-    this._readyResolve = null
     this.blobServerPort = null
+    this._protocolUnsubscribes = []
   }
 
   async initialize() {
@@ -27,7 +38,9 @@ class WorkerClient {
       return this._initPromise
     }
 
-    this._initPromise = this._doInitialize()
+    this._initPromise = this._doInitialize().finally(() => {
+      this._initPromise = null
+    })
     return this._initPromise
   }
 
@@ -40,9 +53,10 @@ class WorkerClient {
       : pipe()
     if (!this.pipe) throw new Error('Failed to create pear pipe')
 
-    // Create HRPC instance with the pipe
-    console.log('[WorkerClient] Creating HRPC instance...')
-    this.rpc = new HRPC(this.pipe)
+    // Create shared protocol client on top of the pipe.
+    console.log('[WorkerClient] Creating shared protocol client...')
+    this.client = createProtocolClient({ stream: this.pipe })
+    this.rpc = this.client.rpc
     console.log('[WorkerClient] HRPC client initialized')
     
     // Debug: log available methods
@@ -51,53 +65,7 @@ class WorkerClient {
     console.log('[WorkerClient] Available RPC methods:', methods.join(', '))
     console.log('[WorkerClient] Has globalSearchVideos:', typeof this.rpc.globalSearchVideos)
 
-    // Add error handler on the RPC
-    if (this.rpc.on) {
-      this.rpc.on('error', (err) => {
-        console.error('[WorkerClient] RPC error:', err)
-      })
-    }
-
-    // Register event handlers (events are best-effort; do not rely on eventReady,
-    // since main-process backend can start before UI loads and events can be missed).
-    this.rpc.onEventReady?.((data) => {
-      console.log('[WorkerClient] Received eventReady:', data)
-    })
-
-    this.rpc.onEventError((data) => {
-      console.error('[WorkerClient] Backend error:', data?.message)
-    })
-
-    this.rpc.onEventUploadProgress((data) => {
-      console.log('[WorkerClient] Upload progress:', data?.progress, '%')
-      window.dispatchEvent(new CustomEvent('pearUploadProgress', { detail: data }))
-    })
-
-    this.rpc.onEventFeedUpdate((data) => {
-      console.log('[WorkerClient] Feed update:', data?.action)
-      window.dispatchEvent(new CustomEvent('pearFeedUpdate', { detail: data }))
-    })
-
-    this.rpc.onEventVideoStats((data) => {
-      console.log('[WorkerClient] Video stats event received:', data?.stats?.progress + '%')
-      window.dispatchEvent(new CustomEvent('pearVideoStats', { detail: data }))
-    })
-
-    this.rpc.onEventCastDeviceFound?.((data) => {
-      window.dispatchEvent(new CustomEvent('pearCastDeviceFound', { detail: data }))
-    })
-
-    this.rpc.onEventCastDeviceLost?.((data) => {
-      window.dispatchEvent(new CustomEvent('pearCastDeviceLost', { detail: data }))
-    })
-
-    this.rpc.onEventCastPlaybackState?.((data) => {
-      window.dispatchEvent(new CustomEvent('pearCastPlaybackState', { detail: data }))
-    })
-
-    this.rpc.onEventCastTimeUpdate?.((data) => {
-      window.dispatchEvent(new CustomEvent('pearCastTimeUpdate', { detail: data }))
-    })
+    this.#bindProtocolEvents()
 
     this.pipe.on('end', () => {
       console.log('[WorkerClient] Pipe ended')
@@ -108,30 +76,95 @@ class WorkerClient {
       console.error('[WorkerClient] Pipe error:', err)
     })
 
-    // Handshake: request status to confirm readiness (and get blob server port).
-    const statusPromise = this.rpc.getStatus({})
-    const status = await Promise.race([
-      statusPromise,
+    // Handshake: request status through the shared protocol client.
+    const ready = await Promise.race([
+      this.client.ready(),
       new Promise((_, reject) => setTimeout(() => reject(new Error('RPC init timeout')), 15000))
     ])
-    const blobServerPort = status?.status?.blobServerPort || null
     this.isConnected = true
-    this.blobServerPort = blobServerPort
+    this.blobServerPort = ready?.blobServerPort ?? null
     console.log('[WorkerClient] Connected. blobServerPort:', this.blobServerPort)
-    this._initPromise = null
+  }
+
+  #bindProtocolEvents() {
+    this.#clearProtocolSubscriptions()
+
+    this._protocolUnsubscribes = [
+      this.client.events.on(PROTOCOL_EVENTS.HOST_ERROR, (data) => {
+        console.error('[WorkerClient] Backend error:', data?.message)
+      }),
+      this.client.events.on(PROTOCOL_EVENTS.LOG, (data) => {
+        const message = data?.message ?? data
+        console.log('[WorkerClient] Backend log:', message)
+      }),
+      this.client.events.on(PROTOCOL_EVENTS.TRANSPORT_CLOSED, (data) => {
+        this.isConnected = false
+        console.log('[WorkerClient] Transport closed:', data?.reason || 'unknown')
+      })
+    ]
+
+    for (const [eventName, domEventName] of Object.entries(DOM_EVENT_NAMES)) {
+      this._protocolUnsubscribes.push(
+        this.client.events.on(eventName, (data) => {
+          if (domEventName === 'pearUploadProgress') {
+            console.log('[WorkerClient] Upload progress:', data?.progress, '%')
+          }
+          if (domEventName === 'pearFeedUpdate') {
+            console.log('[WorkerClient] Feed update:', data?.action)
+          }
+          if (domEventName === 'pearVideoStats') {
+            console.log('[WorkerClient] Video stats event received:', data?.stats?.progress + '%')
+          }
+          window.dispatchEvent(new CustomEvent(domEventName, { detail: data }))
+        })
+      )
+    }
+  }
+
+  #clearProtocolSubscriptions() {
+    for (const unsubscribe of this._protocolUnsubscribes) {
+      try {
+        unsubscribe()
+      } catch {}
+    }
+    this._protocolUnsubscribes = []
+  }
+
+  async connect() {
+    await this.initialize()
+
+    if (!this.pipe || !this.client) {
+      throw new Error('Worker client transport is not initialized')
+    }
+
+    return {
+      stream: this.pipe,
+      client: this.client,
+      terminate: async () => {
+        this.close()
+      }
+    }
   }
 
   getRpc() {
     return this.rpc
   }
 
+  getClient() {
+    return this.client
+  }
+
   close() {
+    this.#clearProtocolSubscriptions()
     if (this.pipe) {
       this.pipe.destroy()
-      this.pipe = null
-      this.rpc = null
-      this.isConnected = false
     }
+    this.pipe = null
+    this.client = null
+    this.rpc = null
+    this.isConnected = false
+    this.blobServerPort = null
+    this._initPromise = null
   }
 }
 

d120c349558d0fa28b9576cd0fc4677eff88a0a1
Developer <dev@localhost>
2026-03-20 13:12:42 -0400

    chore: scaffold host and protocol packages

--- packages/protocol/src/index.js
+++ packages/protocol/src/index.js
@@ -1,3 +1,2 @@
-export * from './event-map.js'
-export * from './create-client.js'
-export { HOST_ERROR_CODES, PROTOCOL_VERSION } from '@peartube/host'
+// Shared protocol exports are added in later tasks.
+export {}
